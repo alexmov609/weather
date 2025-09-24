38d57ef6c5c1bc2ff747e31f4a35ded48b9c2f74
alexmovchan100 <alex.m@esn.co.il>
2025-09-24 16:37:52 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-[url(./assets/images/main.png)] flex items-center justify-center">
+      <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full mx-4">
+        <h1 className="text-1xl font-bold text-red-600 text-center mb-4">
+          Weather App
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
